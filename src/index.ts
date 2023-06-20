0d76c1e0a935c24241d1fc5103c2feceab3a6283
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2023-06-20 17:16:28 +0200

    feat: app exits if no env vars configured

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+import "dotenv/config";
 import app from "./app";
 import "./db";
 
+const requiredEnvVars = ["ACCESS_TOKEN_SECRET", "DB_HOST"];
+
 const { PORT = 3000 } = process.env;
+
+const missingEnvVars = requiredEnvVars.filter(
+  (envVar) => !(envVar in process.env)
+);
+
+if (missingEnvVars.length > 0) {
+  console.error(
+    `Missing required environment variables: ${missingEnvVars.join(", ")}`
+  );
+  process.exit(1);
+}
 
 app.listen(PORT, () => {
   console.log(`> Server running on port ${PORT} 🚀`);
